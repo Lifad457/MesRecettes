d443d7c24c3dca106ada507441b57853a68d5311
Lifad457 <cedric.bison@gmail.com>
2018-10-15 10:51:20 +0200

    15.10.18.10:51

--- src/Form/IngredientType.php
+++ src/Form/IngredientType.php
@@ -5,6 +5,9 @@ use App\Entity\Ingredient;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 class IngredientType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
